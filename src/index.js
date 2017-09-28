9f6bfe6e00bb2e218474ecc854006e6a0cc70718
Ekaterina McArenko <polumna-la@mail.ru>
2017-09-29 01:00:29 +0300

    FIN

--- src/index.js
+++ src/index.js
@@ -11,35 +11,65 @@ a = first.split('');
   temp.reverse().forEach(function(val,ind,arr){
    arr[ind] = String(val);
    if(arr[ind+1] && arr[ind].length > 1){
-    arr[ind+1] += +arr[ind][0];
-    arr[ind] = arr[ind][1];
-   }
+    arr[ind+1] += +arr[ind].slice(0,-1);
+    arr[ind] = arr[ind].slice(-1);
+   } else if(!arr[ind+1] && arr[ind].length > 1){
+    arr[ind+1] = +arr[ind].slice(0,-1);
+    arr[ind] = arr[ind].slice(-1);
+   }   
   })
   res.push(temp.reverse().join(''));
   temp = [];  
   a.splice(a.length-1);
  } 
  
+
+ 
  for(i=0;i<res.length;i++){
- res[i] = '0'.repeat(res.length - 1 - i) + res[i] + '0'.repeat(i)
+ res[i] = res[i] + '0'.repeat(i) 
+ res[i] = res[i].split('').reverse().join('')
  }
  
+
+
  var sum = [];
+
+console.log(res)
  
- for(i=res[0].length-1;i>=0;i--){
+for(var j=res[res.length-1].length-1;j>=0;j--){
+
+ var tmp = 0
+ for(var i=res.length-1;i>=0;i--){  
+  tmp += +res[i][j] || 0;
+ }
+ sum.unshift(tmp)
+ console.log(sum)
+}
+
+ 
+ 
+ 
+ /*
+ for(i=res.length-1;i>=0;i--){
  let tmp = 0;
  for(j=0;j<res.length;j++){
-  tmp+= +res[j][i]
+  tmp += +res[j][i] || +'0';
+  
  }
  sum.push(tmp)
  }
+ */
+
 
  sum.forEach(function(val,ind,arr){
  arr[ind] = String(val);
  if(arr[ind+1] && arr[ind].length > 1){
-  arr[ind+1] += +arr[ind][0];
-  arr[ind] = arr[ind][1];
-  }
+    arr[ind+1] += +arr[ind].slice(0,-1);
+    arr[ind] = arr[ind].slice(-1);
+   } else if(!arr[ind+1] && arr[ind].length > 1){
+    arr[ind+1] = +arr[ind].slice(0,-1);
+    arr[ind] = arr[ind].slice(-1);
+   }
  }
  )
  
